dbd15121f7e4d70892f81fd4f56ad82cbba05560
Kabanchik86 <anton66656@yandex.ru>
2019-03-25 21:05:37 +0300

    25.03 Sedona

--- js/script.js
+++ js/script.js
@@ -1,4 +1,9 @@
 var link = document.querySelector(".second-main-search-descript .butt");
-var popup = document.querySelector (".container");
+var popup = document.querySelector(".container");
 var date = document.querySelector(".first-forms");
-link.addEventListener("click", function (evt) {evt.preventDefault(); popup.classList.toggle("modal-show"); date.focus();})
+
+link.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  popup.classList.toggle("modal-show");
+  date.focus();
+})
